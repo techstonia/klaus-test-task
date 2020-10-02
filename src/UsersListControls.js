78f35bba1e88c9a14b256fa6a3c02af9160622b4
techstonia <techstonia@gmail.com>
2020-10-02 21:17:15 +0300

    Add second row of controls.

--- src/UsersListControls.js
+++ src/UsersListControls.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {getMarkedUsersCount} from './redux/usersSlice';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {colors} from './styleConstants';
 import {DeleteButton, EditButton} from './Buttons';
+import Checkbox from './Checkbox';
+import {ReactComponent as ArrowDown} from './assets/arrow-down.svg';
 
 const Container = styled.div`
   height: 78px;
@@ -26,14 +28,77 @@ const StyledEditButton = styled(EditButton)`
   margin: 0 8px;
 `;
 
+const rowCSS = `
+  position: relative;
+  display: flex;
+`;
+
+const FirstRow = styled.div`
+  ${rowCSS}
+  align-items: center;
+`;
+
+const SecondRow = styled.div`
+  ${rowCSS}
+  margin-top: 25px;
+  align-items: end;
+`;
+
+const Arrow = styled(ArrowDown)`
+  position: relative;
+  left: 4px;
+  top: 2px;
+  ${(props) => props.sorted === "desc" && css`
+    transform: rotate(180deg);
+  `}
+`;
+
+const ColumnHeaderContainer = styled.span`
+  position: absolute;
+  font-weight: 500;
+  font-size: 12px;
+  color: ${colors.gray60};
+  cursor: pointer;
+`;
+
+const ColumnHeader = (props) => {
+  return (
+    <ColumnHeaderContainer className={props.className}>
+      {props.title}
+      {props.sorted && <Arrow sorted={props.sorted} />}
+    </ColumnHeaderContainer>
+  );
+};
+
+const UserColumnHeader = styled(ColumnHeader)`
+  left: 28px;
+`;
+
+const PermissionColumnHeader = styled(ColumnHeader)`
+  left: 385px;
+`;
+
 function UsersListControls() {
   const count = useSelector(getMarkedUsersCount);
 
   return (
     <Container>
-      <UsersCount>{count} users selected</UsersCount>
-      <StyledEditButton text="Edit" />
-      <DeleteButton text="Delete" />
+      <FirstRow>
+        <UsersCount>{count} users selected</UsersCount>
+        <StyledEditButton text="Edit" />
+        <DeleteButton text="Delete" />
+      </FirstRow>
+
+      <SecondRow>
+        <Checkbox />
+        <UserColumnHeader
+          title="User"
+        />
+        <PermissionColumnHeader
+          title="Permission"
+          sorted="desc"
+        />
+      </SecondRow>
     </Container>
   );
 }
