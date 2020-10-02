af12d931bbe8c50fb7497acc005352fe18f1793a
techstonia <techstonia@gmail.com>
2020-10-02 11:28:47 +0300

    Introduce UsersListControls.

---
+++ src/UsersListControls.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 102px;
+`;
+
+function UsersListControls() {
+  return (
+    <Container>
+      UsersListControls
+    </Container>
+  );
+}
+
+export default UsersListControls;
