f6c318a55fbf2ccd4cde5bca895f739a3dfd4708
techstonia <techstonia@gmail.com>
2020-10-02 14:41:00 +0300

    Introduce Buttons.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import Header from './Header';
 import {scrollableAreaWidth} from './styleConstants';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  body, button {
     margin: 0;
     padding: 0;
     background: #EDF2F7;
     font-family: -apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+  
+  button {
+    outline:none;
+    background: none;
   }
 `;
 
---
+++ src/Buttons.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from 'styled-components';
+import {ReactComponent as Pen} from './assets/pen.svg';
+import {ReactComponent as Trash} from './assets/trash.svg';
+
+const background = "white";
+const squareSideLength = 30;
+const borderWidth = 1;
+
+const Button = styled.button.attrs({
+  type: "button",
+})`
+  display: inline-flex;
+  align-items: center;
+
+  height: ${squareSideLength + 2 * borderWidth}px;
+  min-width: ${squareSideLength + 2 * borderWidth}px;
+  cursor: pointer;
+  background: ${background};
+  
+  border: ${borderWidth}px solid #E2E8F0;  
+  box-shadow: 0 1px 2px rgba(45, 55, 72, 0.08);
+  border-radius: 4px;
+`;
+
+const StyledIcon = (Component) => styled(Component)`
+  width: ${squareSideLength}px;
+`;
+
+const Text = styled.span`
+  color: #4A5568;
+  font-weight: 500;
+  font-size: 14px;
+  margin: 0 12px 0 1px;
+  line-height: ${squareSideLength}px;
+`;
+
+const buttonWithIcon = (IconComponent) => {
+  const Icon = StyledIcon(IconComponent);
+
+  return (props) => (
+    <Button className={props.className}>
+      <Icon />
+      {props.text && <Text>{props.text}</Text>}
+    </Button>
+  );
+};
+
+export const DeleteButton = buttonWithIcon(Trash);
+export const EditButton = buttonWithIcon(Pen);
