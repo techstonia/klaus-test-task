e32fd2001ae1d22230deb1551bdd534861a86247
techstonia <techstonia@gmail.com>
2020-10-02 11:30:39 +0300

    Style App component.
    Also include <Header /> component in it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
+import Header from './Header';
+import {scrollableAreaWidth} from './styleConstants';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #EDF2F7;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const Container = styled.div`
+  width: ${scrollableAreaWidth}px;
+  margin: 32px auto;
+`;
 
 function App() {
   return (
-    <>
-      Hello!
-    </>
+    <Container>
+      <GlobalStyle />
+      <Header />
+    </Container>
   );
 }
 
