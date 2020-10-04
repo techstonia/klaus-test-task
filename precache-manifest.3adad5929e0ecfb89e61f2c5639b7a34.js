2269cfb3eb099f1003a8c6d47349c84da2aade84
techstonia <techstonia@gmail.com>
2020-10-04 15:32:24 +0300

    Updates

---
+++ precache-manifest.3adad5929e0ecfb89e61f2c5639b7a34.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1421a83dc094b041366ae309442891ef",
+    "url": "/klaus-test-task/index.html"
+  },
+  {
+    "revision": "29dab8504c1d83dc1ac8",
+    "url": "/klaus-test-task/static/js/2.7b8fbb48.chunk.js"
+  },
+  {
+    "revision": "aee4267aaee9001664159d4d88b44bd8",
+    "url": "/klaus-test-task/static/js/2.7b8fbb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81997929637b6a112871",
+    "url": "/klaus-test-task/static/js/main.df465b3b.chunk.js"
+  },
+  {
+    "revision": "8ea3a0373191c6f566b4",
+    "url": "/klaus-test-task/static/js/runtime-main.c2a1cd20.js"
+  },
+  {
+    "revision": "a283da60fe9c151e6757f6960c30ae5a",
+    "url": "/klaus-test-task/static/media/arrow-down.a283da60.svg"
+  },
+  {
+    "revision": "1c18877689c4c81e3d208de0c6eaf693",
+    "url": "/klaus-test-task/static/media/checkmark.1c188776.svg"
+  },
+  {
+    "revision": "c434f4222626956df30641fab466a744",
+    "url": "/klaus-test-task/static/media/hourglass.c434f422.svg"
+  },
+  {
+    "revision": "4d4150f33b833949f7d9c9fe40f978f5",
+    "url": "/klaus-test-task/static/media/pen.4d4150f3.svg"
+  },
+  {
+    "revision": "431da9f1f73b424da6659c026f8d1fe5",
+    "url": "/klaus-test-task/static/media/trash.431da9f1.svg"
+  }
+]);
