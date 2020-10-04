916d05e1cde504c78e94ac07165c7e21c8434158
techstonia <techstonia@gmail.com>
2020-10-04 11:56:59 +0300

    Add "connect users" button.

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import {colors} from './styleConstants';
 
 const height = 40;
 
 const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
   height: ${height}px;
   margin-bottom: 18px;
 `;
@@ -15,10 +18,29 @@ const Title = styled.span`
   font-weight: 500;
 `;
 
+const ConnectUsersButton = styled.button.attrs({
+  type: "button",
+})`
+  height: ${height}px;
+  min-width: 122px;
+  line-height: ${height}px;
+  cursor: pointer;
+  background: ${colors.brand};
+  color: white;
+
+  border-radius: 4px;
+  border: none;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
 function Header() {
   return (
     <Container>
       <Title>Account users</Title>
+      <span>
+        <ConnectUsersButton>Connect users</ConnectUsersButton>
+      </span>
     </Container>
   );
 }
